30d701fba44441367a7eef84855c26c18acd7aad
DillonVantra <dillonleon82@gmail.com>
2023-10-04 15:50:25 +0700

    enhance

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      backgroundImage: {
+        "gedung-penus": "url('/img/gedung-penus.jpg')",
       },
     },
   },
